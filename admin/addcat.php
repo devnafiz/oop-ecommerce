79369b6cf3da2d0b55c9d03ac97e2d7c69743edd
devnafiz <nafiz.2011@yahoo.com>
2022-04-12 23:24:26 +0600

    add category

--- admin/addcat.php
+++ admin/addcat.php
@@ -1,14 +1,36 @@
-﻿<?php include 'inc/header.php';?>
+﻿
+<?php include '../classes/Category.php';?>
+
+<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
+
+<?php
+
+  $cat =new Category();
+
+  if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    $name =$_POST['name'];
+     $insertCat =$cat->catInsert($name);
+  }
+
+
+?>
         <div class="grid_10">
             <div class="box round first grid">
                 <h2>Add New Category</h2>
+                <?php
+                       if(isset($insertCat)){
+
+                        echo $insertCat;
+                       }
+                ?>
                <div class="block copyblock"> 
-                 <form>
+                 <form action="addcat.php" method="POST">
                     <table class="form">					
                         <tr>
                             <td>
-                                <input type="text" placeholder="Enter Category Name..." class="medium" />
+                                <input type="text" placeholder="Enter Category Name..." class="medium" name="name" />
                             </td>
                         </tr>
 						<tr> 
---
+++ classes/Category.php
@@ -0,0 +1,57 @@
+
+<?php
+
+
+   include_once '../lib/Database.php';
+   include_once '../helpers/Format.php';
+ 
+?>
+
+<?php
+
+ /**
+  * category
+  * 
+  */
+ class Category {
+ 	private $db;
+ 	private $fm;
+ 	
+      public function __construct()
+	 	{
+	 		$this->db = new Database();
+	 		$this->fm =new Format();
+	 	}
+
+ 	public function catInsert($name){
+
+ 		  $name =$this->fm->validation($name);
+         
+          $name =mysqli_real_escape_string($this->db->link, $name);
+
+          if(empty($name)){
+
+          	 $msg='Cat Name Must not empty';
+          	 return $msg;
+          }else{
+
+          	$query ="INSERT INTO tbl_category(name) VALUES ('$name') ";
+
+          	 $catInsert =$this->db->insert($query);
+
+          	 if($catInsert){
+          	 	$msg = 'Category inserted Succefully';
+          	 	return $msg;
+          	 }else{
+
+              $msg = '<span class="error">Category not inserted</span> ';
+          	 	return $msg;
+          	 }
+          }
+
+ 	}
+ }
+
+
+
+?>
